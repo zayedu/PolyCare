70cdbb9dcbcc8e9abe72dbd9ab79b341ebcc0651
zayedu <umerzayed1@gmail.com>
2025-03-31 23:12:57 -0400

    feat: finish ultrasound upload frontend

--- pcos-app/app/index.tsx
+++ pcos-app/app/index.tsx
@@ -1,23 +1,21 @@
-import * as React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import * as React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import SymptomUploadScreen from './SymptomUploadScreen';
-import HomeScreen from './HomeScreen';
+import SymptomUploadScreen from "./SymptomUploadScreen";
+import UltrasoundUploadScreen from "./UltrasoundUploadScreen";
+import HomeScreen from "./HomeScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function Index() {
-  
   return (
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-        />
-        <Stack.Screen
-          name="SymptomUpload"
-          component={SymptomUploadScreen}
-        />
-      </Stack.Navigator>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="SymptomUpload" component={SymptomUploadScreen} />
+      <Stack.Screen
+        name="UltrasoundUpload"
+        component={UltrasoundUploadScreen}
+      />
+    </Stack.Navigator>
   );
 }
