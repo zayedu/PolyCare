16e9558e18bb4ae60827185765d1ff274e6262c5
zayedu <umerzayed1@gmail.com>
2025-04-01 15:39:20 -0400

    test: ultrasound calc

--- backend/test.py
+++ backend/test.py
@@ -3,6 +3,8 @@
 from flask import json
 from calculateResults.symptomCalculation import symptomCalculation
 from backend.uploadResults.discussSymptoms import discussSymptoms
+from backend.uploadResults.discussUltrasound import discussUltrasound
+from calculateResults.UltrasoundCalculation import UltrasoundCalculation
 
 # Test to upload data into uploadData.csv
 discussSymptoms(json.dumps({"test": "random data input"})).uploadUserSymptom()
@@ -12,3 +14,8 @@ print(symptomCalculation().getUserSymptoms())
 
 # Test for calculating PCOS based off data stored in uploadData.csv
 print("Probablity: ",symptomCalculation().calculatePCOSFromSymptom())
+
+# assume image stored in db already
+ultrasoundCalculation = UltrasoundCalculation()
+result = ultrasoundCalculation.predict()
+print(result)
