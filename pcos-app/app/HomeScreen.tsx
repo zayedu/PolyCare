706ec69244d5e7cd9e8d2f8cb04d476d9cb50299
Amal Hamid <amalhamid@Amals-MacBook-Air-4.local>
2025-04-03 11:17:23 -0400

    error in homescreen fix

--- pcos-app/app/HomeScreen.tsx
+++ pcos-app/app/HomeScreen.tsx
@@ -1,16 +1,5 @@
 import { View, Text, Button } from "react-native";
 
-<<<<<<< HEAD
-export default function HomeScreen( {navigation}: {navigation: any} ){
-    return (
-        <View >
-            <Text>Home Screen</Text>
-            <Button title="Upload Symptom Results" onPress={() => navigation.navigate("SymptomUpload")} />
-            <Button title="Upload Blood Test Results" onPress = {() => navigation.navigate("BloodUpload")} />
-        </View>
-    )
-}
-=======
 export default function HomeScreen({ navigation }: { navigation: any }) {
   return (
     <View style={{ flex: 1, marginTop: 20, marginBottom: 20 }}>
@@ -22,7 +11,10 @@ export default function HomeScreen({ navigation }: { navigation: any }) {
         title="Upload Ultrasound"
         onPress={() => navigation.navigate("Ultrasound Upload Page")}
       />
+      <Button
+        title="Upload Blood Test Results"
+        onPress={() => navigation.navigate("Blood Test Upload Page")}
+      />
     </View>
   );
 }
->>>>>>> main
